f2041dbd2b4a622617f29c8e0bf688a0fb91d93d
Roice Johnson <rjohn230uncc.edu>
2020-01-31 14:40:32 -0500

    first commit

---
+++ src/main/java/com/jsware/voteAdmin/VoteAdminApplication.java
@@ -0,0 +1,13 @@
+package com.jsware.voteAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsware/voteAdmin/VoteAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsware.voteAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
